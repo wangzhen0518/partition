20edff62f726781943f30fe6ca77ba4730015a13
wangzhen0518 <wangzhen0518@126.com>
2023-04-01 14:36:33 +0800

    fix code of generate hg

--- run_test.py
+++ run_test.py
@@ -17,14 +17,18 @@ def generate_hg_file(src_dir, dst_dir):
     """
     subprocess.getstatusoutput(f"mkdir -p {dst_dir}")
     file_list = glob.glob(f"{src_dir}/*.json")
+    try:
+        file_list.remove("config.json")
+    except ValueError:
+        pass
     # file_list = [f"{src_dir}/adaptec3.json", f"{src_dir}/adaptec4.json", f"{src_dir}/bigblue2.json"]
     for file in file_list:
         generate_single_hg_file(file, os.path.join(dst_dir, del_ext_name(file) + ".hg"))
 
 
 if __name__ == "__main__":
-    config_pth='./test/ispd2005'
-    hg_pth='./benchmarks/ispd2005/hypergraph'
+    config_pth = "./test/ispd2005"
+    hg_pth = "./benchmarks/ispd2005/hypergraph"
     generate_hg_file(config_pth, hg_pth)
     shmetis = shmetis_method("ispd2005", False)
     shmetis.run_all(8)
